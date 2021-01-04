a21c64fe0f0d10aaa3ea6e39af44848f46e8f877
Shanan <Mohamed.shanan48@gmail.com>
2021-01-04 10:05:47 +0200

    Adding project files

---
+++ accounts/models.py
@@ -0,0 +1,58 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.base import Model
+from django.utils.translation import ugettext_lazy as _
+from django.db.models.signals import post_save
+from django.utils.text import slugify
+
+
+# Create your models here.
+class Profile(models.Model):
+    """Model definition for Profile."""
+    
+    user  = models.OneToOneField( User, verbose_name=_("user"), on_delete=models.CASCADE)
+    name = models.CharField(_("الاسم: "), max_length=50)
+    subtitle = models.CharField(_("نبذه عنك: "), max_length=50)
+    adress = models.CharField(_("المحافظة: "), max_length=50)
+    adress_details = models.CharField(_("العنوان بالتفصيل: "), max_length=50)
+    phone_number = models.CharField(_("الهاتف"), max_length=50)
+    working_hours = models.CharField(_("عدد ساعات العمل: "), max_length=50)
+    waiting_time = models.IntegerField(_("مدة الانتظار "),blank=True, null=True)
+    doctor = models.CharField(_("دكتور ؟"), max_length=50,blank=True, null=True)
+    doctor_specialist = models.CharField(_("متخصص في؟"), max_length=50,blank=True, null=True)
+    who_i = models.TextField(_("من أنا: "), max_length=50)
+    price = models.IntegerField(_("سعر الكشف"),blank=True, null=True)
+    facebook = models.CharField(max_length=50,blank=True, null=True)
+    twittre = models.CharField(max_length=50,blank=True, null=True)
+    google = models.CharField(max_length=50,blank=True, null=True)
+    image = models.ImageField(_("الصورة الشخصية"), upload_to='profile',blank=True, null=True)
+    slug = models.SlugField(_("slug"), blank=True, null=True)
+    
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.user.username)
+        
+        super(Profile,self).save(*args,**kwargs)
+    # TODO: Define fields here
+
+    class Meta:
+        """Meta definition for Profile."""
+
+        verbose_name = 'Profile'
+        verbose_name_plural = 'Profiles'
+
+    def __str__(self):
+        """Unicode representation of Profile."""
+        return '%s' %(self.user.username)
+
+
+def creat_profile(sender, **kwargs):
+    
+    if kwargs['created']:
+        Profile.objects.create(user = kwargs['instance'])
+
+
+post_save.connect(creat_profile, sender= User)      
+
+    # TODO: Define custom methods here
