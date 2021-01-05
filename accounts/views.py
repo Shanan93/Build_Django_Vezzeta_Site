df3ca93abd4ad87d9c352554048d49914773ec22
Shanan <Mohamed.shanan48@gmail.com>
2021-01-05 11:22:18 +0200

    Installing Crsipy and Bootstrap then creating login page

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.contrib.auth.models import User
 from .models import Profile
+from .forms import Login_Form
+from django.contrib.auth import  authenticate, login
 
 # Create your views here.
 def doctors_list(request):
@@ -13,3 +15,20 @@ def doctors_detail(request, slug):
     doctors_detail = Profile.objects.get(slug = slug) # Query set
     
     return render(request, 'user/doctors_detail.html', {'doctors_detail': doctors_detail })
+
+
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username = username, password = password)
+        if user is not None:
+            login(request,user)
+            return redirect('accounts:doctors_list',)
+        
+    
+    form = Login_Form()
+    
+    return render(request, 'user/login.html', {'form': form })
+    
--- src/settings.py
+++ src/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'accounts'
+    'accounts',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -51,6 +52,7 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'src.urls'
+CRISPY_TEMPLATE_PACK = 'bootstrap4' 
 
 TEMPLATES = [
     {
